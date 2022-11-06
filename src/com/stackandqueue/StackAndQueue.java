7cb6b481321effdd9aa72258cf65d4c2feec6a37
Kajakwankhede <Kajalwankhede@example.com>
2022-11-06 13:30:25 +0530

    UC2- Ability to peak and pop from the stack till it is empty

--- src/com/stackandqueue/StackAndQueue.java
+++ src/com/stackandqueue/StackAndQueue.java
@@ -1,29 +1,60 @@
 package com.stackandqueue;
 import static java.lang.System.exit;
 public class StackAndQueue {
-        private class Node { //Linked list node
-            int data; // integer data
-            Node next; // reference variable Node type
+    private class Node { //Linked list node
+        int data; // integer data
+        Node next; // reference variable Node type
+    }
+
+    Node top;     // create global top reference variable global
+
+    StackAndQueue() {
+        this.top = null;
+    }
+
+    public void push(int a) // insert at the beginning
+    {
+        Node temp = new Node();     // create new node temp and allocate memory
+        if (temp == null) {
+            System.out.print("\nHeap Overflow");
+            return;
         }
-          Node top;     // create global top reference variable global
-        StackAndQueue() {
-            this.top = null;
+        temp.data = a;
+        temp.next = top;
+        top = temp;
+    }
+
+    boolean isEmpty() {  //check if stack is empty or not
+        return top == null;
+    }
+
+
+    public int peek()  //return top element in a stack
+
+    {
+        if (!isEmpty()) {  // If stack is not empty , return the top element
+            return top.data;
+
+        } else
+            System.out.println("Stack is empty");
+
+        return -1;
+    }
+
+    void pop()// Remove a key from given queue
+    {
+        if (top == null) {
+            System.out.println("\n Stack is empty");
+            return;
         }
-        public void push(int a) // insert at the beginning
-        {
-            Node temp = new Node();     // create new node temp and allocate memory
-            if (temp == null) {
-                System.out.print("\nHeap Overflow");
-                return;
-            }
-            temp.data = a;
-            temp.next = top;
-            top = temp;
-        }
-        public void printData()
+        top = top.next;
+
+    }
+
+    public void printData()
         {
             if (top == null) {
-                System.out.printf("\nStack Underflow");
+                System.out.printf("Stack is empty");
                 exit(1);
             }
             else {
@@ -44,6 +75,14 @@ public class StackAndQueue {
         obj.push(70);
         obj.push(30);
         obj.push(56);
+        System.out.println("\nAdding Element in Stack: ");
+        obj.printData();
+        System.out.printf("\n\nTop of Element is: %d\n",obj.peek());
+        obj.printData();
+        System.out.println("\n\nRemoving Element in Stack: ");
+        obj.pop();
+        obj.pop();
+        obj.pop();
         obj.printData();
 
 
