f9c1f130d992f0f1cdd4fcb56c1da35873dd3409
Kajakwankhede <Kajalwankhede@example.com>
2022-11-06 14:35:11 +0530

    Queue Using linked list

---
+++ src/com/stackandqueue/QueueOperation.java
@@ -0,0 +1,10 @@
+package com.stackandqueue;
+
+public class QueueOperation {
+
+
+    public static void main(String[] args) {
+        System.out.println("Implementation of Queue Using Linked List");
+    }
+}
+
